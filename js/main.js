594072ef76cc411cb8240959d73b2c9c545c53c9
ahmed-yasser12 <ahmedyasser541@gmail.com>
2024-07-15 21:41:57 +0300

    fokir project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".type", {
+    strings: [" Designer ", "Developer"],
+    typeSpeed: 90, // Default value
+    backSpeed: 110, // Default value
+    loop: true, // Default value
+  });
+
+let navbar =document.getElementById("navBar");
+console.log(navbar);
